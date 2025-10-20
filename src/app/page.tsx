0168c74201cea997aeb603e92175563c77634d7b
Avabucks <avabuckssociety@gmail.com>
2025-10-20 21:55:09 +0200

    Aggiornamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_PHP_API}/api/test_api.php`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
+  }, []);
+
+
   return (
     <section className="bg-primary text-text-color p-4">
       <h1 className="text-primary">Benvenuto nella Home!</h1>
@@ -6,7 +17,6 @@ export default function Home() {
       <button className="bg-[var(--primary)] text-white rounded-[var(--border-radius)]">
         Cliccami
       </button>
-      <p>{`${process.env.NEXT_PUBLIC_PHP_API}`}</p>
     </section>
   );
 }
