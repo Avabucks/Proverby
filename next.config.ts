8649753e7f8757e28486844cd60fcd6682ca2b9f
Avabucks <avabuckssociety@gmail.com>
2025-10-21 15:53:16 +0200

    Aggiornamento

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const isLocalhost =
+  typeof window !== "undefined" &&
+  (window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1" ||
+    window.location.hostname === "192.168.56.1");
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_PHP_API: isLocalhost
+      ? "http://localhost:8000/api"
+      : "https://proverby.it/api",
+  },
 };
 
 export default nextConfig;
