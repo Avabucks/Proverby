2692ae8e30e4723fce830c20dd797035699ee107
Avabucks <avabuckssociety@gmail.com>
2025-10-23 21:51:09 +0200

    Aggiornamento

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { auth, googleProvider } from "@/src/lib/firebase";
 import { signInWithPopup } from "firebase/auth";
-import { saveUser } from "@/src/app/save_user";
+import { saveUser } from "@/src/actions/save_user";
 import Ripple from "@/src/components/ripple/Ripple";
 import GoogleLogo from "@/public/assets/google_logo.svg";
 import { setCookie } from "cookies-next";
