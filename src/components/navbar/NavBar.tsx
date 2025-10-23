60f9ce7f1a8d3f3dd0ae25d93c12863d82a0b172
Avabucks <avabuckssociety@gmail.com>
2025-10-23 17:29:35 +0200

    Aggiornamento

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -7,42 +7,30 @@ import Logo from "@/public/assets/logo.png";
 
 export default function Navbar() {
   return (
-    <header>
-        <div>
-            <div className="shadow shadow-inactive"></div>
-            <div className="headerLeft">
-              <Ripple icon="toggleMenu bx bx-menu">
+    <nav>
+        <div className="flex items-center justify-between w-full">
+            <div className="flex items-center gap-[20px] ml-[-15px]">
+              <Ripple icon="hidden">
                 <div className="logo">
                     <Link href="/">
                         <Image src={Logo} alt="Logo" width={155} />
                     </Link>
                 </div>
               </Ripple>
-              <div className="nav nav-inactive">
-                <div>
-                  <div className="logo">
-                    <i className="toggleMenu bx bx-menu"></i>
-                      <div>
-                        <Link href="/">
-                          <Image src={Logo} alt="Logo" width={155} />
-                        </Link>
-                      </div>
-                  </div>
-                  <ul className="menu">
-                    <div className="v-divider"></div>
-                      <Link href="/"><Ripple icon="bx bx-gallery-vertical-end">Sfoglia</Ripple></Link>
-                      <Link href="/about"><Ripple icon="bx bx-gallery-vertical-end">About</Ripple></Link>
-                  </ul>
-                </div>
+              <div>
+                <ul className="menu">
+                  <div className="v-divider"></div>
+                    <Link href="/"><Ripple icon="bx bx-gallery-vertical-end">Sfoglia</Ripple></Link>
+                    <Link href="/about"><Ripple icon="bx bx-gallery-vertical-end">About</Ripple></Link>
+                </ul>
               </div>
             </div>
-
             <div className="menu-cont">
                 <div className="ripple outline toggle-theme"><i className='bx'></i></div>
                 <div className="v-divider"></div>
                 <GoogleLogin></GoogleLogin>
             </div>
         </div>
-    </header>
+    </nav>
   );
 }
